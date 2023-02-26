d1c3919e7045a0a4498a729da9550725e72e5d3b
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-26 17:04:36 +0300

    Auto extraction of necessary information

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -12,5 +18,6 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts():
-    return {"message": "successfully created posts"}
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data": "new post"}
