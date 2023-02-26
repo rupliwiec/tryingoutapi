d4e10a4fb073ea36515cb36b0f7f8201fad62ed3
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-26 17:48:27 +0300

    Conversion into dictionary from pydantic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -8,6 +9,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -19,6 +21,7 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post.published)
-    return {"data": "new post"}
+def create_posts(post: Post):
+    print(post.rating)
+    print(post.dict())
+    return {"data": post}
