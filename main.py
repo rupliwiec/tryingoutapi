baae7be3d82746185e4bcd75c02c15cb98701b13
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-26 16:01:04 +0300

    Post request to API

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "welcome to my api"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "successfully created posts"}
