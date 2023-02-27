f3a4f23a9673e376d1f342047658e23f8c7964d6
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-27 17:18:28 +0300

    if not exception

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -38,4 +38,6 @@ def create_posts(post: Post):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found")
     return {"post detail": post}
