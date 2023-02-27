1adf23feef5079dccc1b55885a2192742ba711de
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-27 16:04:48 +0300

    get post id function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,16 +13,24 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts=[{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "I like pizza", "id": 2}]
+
 @app.get("/")
 def root():
     return {"message": "welcome to my api"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post.rating)
-    print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post detail": f"Here is post {id}"}
