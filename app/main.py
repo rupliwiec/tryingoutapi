bbb3cecb81ac946c699e29f62cc5f4d4c0ee3154
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-03-07 16:43:55 +0300

    unpacking dictionary to make it more efficient and less time consuming

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     # new_post = cursor.fetchone()
     # in order to save changes while inserting new data to the database we use .commit
     # conn.commit()
-    new_post = models.Post(title=post.title, content = post.content, published = post.published)
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
