3027a8c4314a480dbe05ac32e9ef545979f03655
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-03-09 19:36:38 +0300

    Create user function

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -16,3 +16,7 @@ class Post(PostBase):
 
     class Config:
         orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
