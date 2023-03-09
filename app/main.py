2ece77145e1560e75f42319ecb07f098248b0e84
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-03-09 11:34:25 +0300

    Delete operation using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -88,16 +88,18 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return {"post detail": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
     
-    cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} does not exist")
     
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    post.delete(synchronize_session=False)
+    db.commit()
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
