3501f769b07efc201d9406930fe01cf92c6a6c53
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-03-10 10:57:57 +0300

    Creating file utils.py for different supporting functions

--- app/main.py
+++ app/main.py
@@ -5,13 +5,10 @@ from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from passlib.context import CryptContext
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -119,7 +116,7 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 def create_user(user: schemas.UserCreate ,db: Session = Depends(get_db)):
 
     # hash the password - user.password
-    hashed_pwd = pwd_context.hash(user.password)
+    hashed_pwd = utils.hash(user.password)
     user.password = hashed_pwd
     new_user = models.User(**user.dict())
     db.add(new_user)
